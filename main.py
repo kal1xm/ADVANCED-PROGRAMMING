2cc2b4089c19484a5f77a2fa0d8aee40b255e1df
Your Name <your.email@example.com>
2026-01-29 18:23:39 +0000

    added close button for all pages

--- main.py
+++ main.py
@@ -45,8 +45,10 @@ class MainWindow(QMainWindow):
         self.release_mgmt = ReleaseManagementWidget()
         self.cost_mgmt = CostManagementWidget()
         self.dashboard.logout.connect(QApplication.quit)   #logout button directly closes the program
-
-       
+        self.licensee_mgmt.logout.connect(QApplication.quit)
+        self.rhu_mgmt.logout.connect(QApplication.quit)
+        self.allocation.logout.connect(QApplication.quit)
+        self.cost_mgmt.logout.connect(QApplication.quit)
         self.stacked_widget.addWidget(self.dashboard)          
         self.stacked_widget.addWidget(self.licensee_mgmt)    
         self.stacked_widget.addWidget(self.rhu_mgmt)           
